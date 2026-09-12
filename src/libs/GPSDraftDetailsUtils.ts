8566c6212611295d5caa524bde54d73207c6c228
Jesus Barrios <jesusggiraldo1999@gmail.com>
2026-09-12 02:00:44 -0500

    fix(GPSDraftDetailsUtils): only discard trailing segment if multiple segments exist (closes #100938)

--- src/libs/GPSDraftDetailsUtils.ts
+++ src/libs/GPSDraftDetailsUtils.ts
@@ -139,7 +139,7 @@ async function stopGpsTrip(isOffline: boolean, gpsPoints: GPSPoint[][], skipLast
         return;
     }
 
-    if (isLastSegmentEmptyOrHasOnlyOnePoint(lastSegment)) {
+    if (gpsPoints.length > 1 && isLastSegmentEmptyOrHasOnlyOnePoint(lastSegment)) {
         removeLastSegment(gpsPoints);
         return;
     }
